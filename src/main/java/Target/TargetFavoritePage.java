d27d89064b1c2784f1565f9444370bbb00644a3e
JoseSolano17 <jsolanom@ucenfotec.ac.cr>
2021-04-03 03:55:19 -0600

    Finihing testing

--- src/main/java/Target/TargetFavoritePage.java
+++ src/main/java/Target/TargetFavoritePage.java
@@ -14,19 +14,19 @@ public class TargetFavoritePage extends BasePage{
     By FavoriteTxt = By.className("Heading__StyledHeading-sc-1mp23s9-0");
 
     public boolean saveFavorite()throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(8000);
         driver.findElement(FavoriteBtn).click();
         return true;
     }
 
     public boolean showFavorite(String savedItem)throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(8000);
         String favoriteSave = driver.findElement(FavoriteSave).getText();
         return favoriteSave.equalsIgnoreCase(savedItem);
     }
 
     public boolean noFavoritesSaved()throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(8000);
         String favorites = driver.findElements(FavoriteTxt).get(2).getText();
         return favorites.equalsIgnoreCase("Show some love!");
     }
